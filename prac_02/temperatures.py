2294e3508f0259657fd3cbd7f049d73c38e1b526
kiruba132 <kirubanithi.sornamoorthy@my.jcu.edu.au>
2024-09-29 19:50:04 +1000

    Add clarity and indentation to temperatures.py

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,43 +1,34 @@
-"""
-CP1404- Practical
-Temperature conversions
-"""
-
-MENU = """C - Convert Celsius to Fahrenheit
-    F - Convert Fahrenheit to Celsius
-    Q - Quit"""
-
-
 def main():
-    """Temperature conversion program."""
+    MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
+
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            fahrenheit = get_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
-            fahrenheit = float(input("Fahrenheit : "))
-            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = get_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
+
         print(MENU)
         choice = input(">>> ").upper()
+
     print("Thank you.")
 
-
-def convert_celsius_to_fahrenheit(celsius):
-    """Convert celsius to fahrenheit."""
+def get_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
     return celsius * 9.0 / 5 + 32
 
-
-def convert_fahrenheit_to_celsius(fahrenheit):
-    """Convert fahrenheit to celsius."""
+def get_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
     return (fahrenheit - 32) * 5 / 9
 
-
+# Call the main function to execute the program
 main()
-
-
